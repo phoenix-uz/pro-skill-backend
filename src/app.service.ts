243a543e4f92a48cc7b05ffa6d6a319338adcf8a
Akbarkhon <akbarkhonavazkhonov@gmail.com>
2024-05-19 19:20:21 +0500

    upload file or image

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class AppService {
-  constructor(private prisma: PrismaService) {}
   getTest(): string {
     return 'Rediska Kolobokova!';
   }
--- src/functions.ts
+++ src/functions.ts
@@ -2,7 +2,15 @@ import { promises as fsPromises } from 'fs';
 export default async function saveFile(file: Express.Multer.File) {
   const randomFileName = Math.random().toString(36).substring(7);
   const extension = file.originalname.split('.').pop();
-  const filePath = `${process.env.UPLOADS_DIR}/${Date.now()}${randomFileName}.${extension}`;
+  const filePath = `dist/${process.env.UPLOADS_DIR}/${Date.now()}${randomFileName}.${extension}`;
+  try {
+    //dir is exist
+    await fsPromises.access(`dist/${process.env.UPLOADS_DIR}`);
+  } catch (e) {
+    //dir is not exist
+    await fsPromises.mkdir(`dist/${process.env.UPLOADS_DIR}`);
+  }
   await fsPromises.writeFile(filePath, file.buffer);
-  return filePath;
+  //return path to file without dist
+  return filePath.slice(5);
 }
