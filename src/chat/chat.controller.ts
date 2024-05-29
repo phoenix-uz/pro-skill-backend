69b44f3800fa19a578598b1e8c7b1c0615f1fb2a
Maftuna Raimova <maftuna.a.raimova@gmail.com>
2024-05-29 11:26:04 +0500

    chat get all / get by id

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,7 +1,30 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { ChatGateway } from './chat.gateway';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ChatService } from './chat.service';
+import { AdminGuard } from 'src/admin/admin.guard';
 
+@ApiTags('Chat')
 @Controller('chat')
 export class ChatController {
-  constructor(private readonly socketGateway: ChatGateway) {}
+  constructor(private readonly socketGateway: ChatGateway , 
+    private readonly chatService: ChatService
+  ) {}
+
+  @UseGuards(AdminGuard)
+  @ApiBearerAuth()
+  @Get('chat')
+  @ApiOperation({ summary: 'Get all chats' })
+  findAll() {
+    return this.chatService.findAll();
+  }
+  
+  @UseGuards(AdminGuard)
+  @ApiBearerAuth()
+  @Get(':id')
+  @ApiOperation({ summary: 'Get chats by id' })
+  findOne(@Param('id') id: number) {
+    return this.chatService.findById(+id);
+  }
+ 
 }
--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class ChatService {}
+export class ChatService {
+    constructor(private readonly prisma: PrismaService) {}
+    
+    async findAll() {
+        const chat = await this.prisma.chat.findMany();
+        return chat;
+    }
+
+    async findById(id: number) {
+        try {
+            const chat = await this.prisma.chat.findUnique({
+                where: { id: id },
+            });
+            
+            if (!chat) {
+                throw new HttpException('Chat not found', HttpStatus.NOT_FOUND);
+            }
+            return chat;
+        } catch (error) {
+            throw new HttpException('An error occurred while retrieving the chat', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
