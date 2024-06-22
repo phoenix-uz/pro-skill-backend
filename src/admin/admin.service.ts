597f993bde36ac68f0ddba35dd27f57d1522dfca
Akbarkhon <akbarkhonavazkhonov@gmail.com>
2024-06-22 17:29:34 +0500

    name != process.env.ADMIN_NAME &&       name != process.env.MENTOR_NAME &&       name != process.env.FINANCE_NAME

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -10,17 +10,45 @@ export class AdminService {
     private jwtService: JwtService,
   ) {}
   async login(name: string, password: string) {
-    if (name != process.env.ADMIN_NAME) {
+    if (
+      name != process.env.ADMIN_NAME &&
+      name != process.env.MENTOR_NAME &&
+      name != process.env.FINANCE_NAME
+    ) {
+      console.log(name != process.env.MENTOR_NAME);
       throw new HttpException('User not found', HttpStatus.NOT_FOUND);
     }
-    if (!(await bcrypt.compare(password, process.env.ADMIN_PASSWORD))) {
-      throw new HttpException('Wrong password', HttpStatus.UNAUTHORIZED);
+    if (name === process.env.ADMIN_NAME) {
+      if (!(await bcrypt.compare(password, process.env.ADMIN_PASSWORD))) {
+        throw new HttpException('Wrong password', HttpStatus.UNAUTHORIZED);
+      }
+      const payload = { sub: 1, name: name };
+      return {
+        access_token: await this.jwtService.signAsync(payload),
+        name: name,
+        role: 'admin',
+      };
+    } else if (name === process.env.MENTOR_NAME) {
+      if (!(await bcrypt.compare(password, process.env.MENTOR_PASSWORD))) {
+        throw new HttpException('Wrong password', HttpStatus.UNAUTHORIZED);
+      }
+      const payload = { sub: 1, name: name };
+      return {
+        access_token: await this.jwtService.signAsync(payload),
+        name: name,
+        role: 'mentor',
+      };
+    } else if (name === process.env.FINANCE_NAME) {
+      if (!(await bcrypt.compare(password, process.env.FINANCE_PASSWORD))) {
+        throw new HttpException('Wrong password', HttpStatus.UNAUTHORIZED);
+      }
+      const payload = { sub: 1, name: name };
+      return {
+        access_token: await this.jwtService.signAsync(payload),
+        name: name,
+        role: 'finance',
+      };
     }
-    const payload = { sub: 1, name: name };
-    return {
-      access_token: await this.jwtService.signAsync(payload),
-      name: name,
-    };
   }
 
   async getStudentCount() {
