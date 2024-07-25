6a092e8850581503035e710fc6dd3db8d3ae0051
Akbarkhon <akbarkhonavazkhonov@gmail.com>
2024-07-25 19:57:55 +0500

    added price

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,17 +3,6 @@ const prisma = new PrismaClient();
 
 async function main() {
   // Users
-  const user1 = await prisma.user.create({
-    data: {
-      fullName: 'John Doe',
-      email: 'john.doe@example.com',
-      phoneNumber: '1234567890',
-      gender: 'Male',
-      city: 'New York',
-      birthday: '1990-01-01',
-      password: 'password123',
-    },
-  });
 
   // Courses
   const course1 = await prisma.courses.create({
@@ -22,23 +11,27 @@ async function main() {
       description: 'Business Administration course description',
       author: 'Author 1',
       time: '10h',
+      price: 100,
       photoUrls: ['url1', 'url2'],
       modules: {
         create: [
           {
             title: 'Moddule1: Introduction to Business Administration',
             time: '2h',
+            price: 50,
             lessons: {
               create: [
                 {
                   title: 'Lesson1: Create a Business Plan',
                   videoUrl: 'video_url_1',
                   time: '1h',
+                  price: 20,
                 },
                 {
                   title: 'Lesson2: Business Administration Basics',
                   videoUrl: 'video_url_2',
                   time: '1h',
+                  price: 20,
                 },
               ],
             },
@@ -46,17 +39,20 @@ async function main() {
           {
             title: 'Module2: Business Administration Advanced',
             time: '8h',
+            price: 100,
             lessons: {
               create: [
                 {
                   title: 'Lesson3: Business Administration Advanced',
                   videoUrl: 'video_url_3',
                   time: '4h',
+                  price: 100,
                 },
                 {
                   title: 'Lesson4: Business Administration Advanced 2',
                   videoUrl: 'video_url_4',
                   time: '4h',
+                  price: 100,
                 },
               ],
             },
